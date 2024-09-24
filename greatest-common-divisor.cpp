b08a27a0b10528fa46f6bc8b6df8fc17c156af9b
frontender <134868734+all3n49@users.noreply.github.com>
2024-09-24 23:57:28 +0530

    Update greatest-common-divisor.cpp

--- greatest-common-divisor.cpp
+++ greatest-common-divisor.cpp
@@ -1,3 +1,11 @@
+//Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+// Example 1:
+//Input: str1 = "ABCABC", str2 = "ABC"
+//Output: "ABC"
+//Example 2:
+//Input: str1 = "ABABAB", str2 = "ABAB"
+//Output: "AB"
+
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
